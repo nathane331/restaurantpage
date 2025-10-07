a8d5763814a8a3ce4421d4575af13a2c258dcaca
nathane331 <nathan.barkerenglish@gmail.com>
2025-10-06 23:15:15 -0400

    Pages completed.

--- src/menupage.js
+++ src/menupage.js
@@ -8,9 +8,38 @@ export function menuLoad(){
     const menuFragment = document.createDocumentFragment();
 
     const menuContent = menuFragment.appendChild(document.createElement("div"));
-    menuContent.textContent = "this is the menu";
+    menuContent.classList.add("menu-content");
+    const menuHeader = menuContent.appendChild(document.createElement("h2"));
+    menuHeader.textContent = "WHAT'S ON TAP";
+    menuHeader.classList.add("menu-header");
+    
 
     content.appendChild(menuFragment);
     console.log("Menu Loaded.");
+
+    const menuList = menuContent.appendChild(document.createElement("ul"));
+    menuList.classList.add("menu-list");
+
+
+
+    const bloods = ["TYPE 0 -", "TYPE 0 +", "TYPE A +","TYPE A -", "TYPE B +", "TYPE B -", "TYPE AB +", "TYPE AB -"];
+    const prices = [12, 10, 10, 16, 18, 18, 22, 22];
+
+    for (let i = 0; i < bloods.length; i++) {
+        
+        let listItem = menuList.appendChild(document.createElement("li"));
+        listItem.classList.add("menu-item");
+
+        let itemName = listItem.appendChild(document.createElement("span"));
+        itemName.textContent = bloods[i];
+        let itemPrice = listItem.appendChild(document.createElement("span"));
+        itemPrice.textContent = prices[i];
+
+
+        
+    }
+    
+    
+
 
 };
